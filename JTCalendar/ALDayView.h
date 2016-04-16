1c741656564e38f0e48f708820b5f4f02dee058e
tercteberc <tercteberc08@rambler.ru>
2016-04-17 00:35:34 +0300

    add custom class dayView

---
+++ JTCalendar/ALDayView.h
@@ -0,0 +1,13 @@
+//
+//  ALDayView.h
+//  Pods
+//
+//  Created by Антон on 17.04.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALDayView : UIView
+
+@end
---
+++ JTCalendar/ALDayView.m
@@ -0,0 +1,18 @@
+//
+//  ALDayView.m
+//  Pods
+//
+//  Created by Антон on 17.04.16.
+//
+//
+
+#import "ALDayView.h"
+
+@implementation ALDayView
+
+- (void)drawRect:(CGRect)rect {
+    
+}
+
+
+@end
